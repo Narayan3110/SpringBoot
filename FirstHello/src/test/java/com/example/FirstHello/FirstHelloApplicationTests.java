1913c43b69ea4d5c177114e91dd8eb93fea8f8bf
Narayan3110 <narayanpandey92200@gmail.com>
2024-12-09 21:59:01 +0530

    First Spring Boot Code
    Simple Start of my learning

---
+++ FirstHello/src/test/java/com/example/FirstHello/FirstHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FirstHello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
